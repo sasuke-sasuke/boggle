a1e5daf3d190351b105cd9516e1deff9b1339ca2
Jeof Petty <jeofpetty@icloud.com>
2023-02-08 00:50:07 -0500

    add timer, score, highscore

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from boggle import Boggle
-from flask import Flask, request, render_template, redirect, session
+from flask import Flask, request, render_template, redirect, session, jsonify
 from flask_debugtoolbar import DebugToolbarExtension
 
 app = Flask(__name__)
@@ -8,17 +8,28 @@ app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 debug = DebugToolbarExtension(app)
 
 boggle_game = Boggle()
-game_board = boggle_game.make_board()
+#game_board = boggle_game.make_board()
 
 @app.route('/')
 def index():
+    game_board = boggle_game.make_board()
     session['boggle_board'] = game_board
-    return render_template('index.html', game_board=game_board)
+    highscore = session.get('highscore', 0)
+    play_throughs = session.get('play_thr', 0)
+    return render_template('index.html', game_board=game_board, highscore=highscore, plays=play_throughs)
 
-@app.route('/guess', methods=['POST'])
-def guess():
-    return redirect('/')
-
-@app.route('/words')
+@app.route('/check_guess')
 def check_guess():
-    return redirect('/')
+    guess = request.args['guess']
+    game_board = session['boggle_board']
+    res = boggle_game.check_valid_word(game_board, guess)
+    return jsonify({ 'result': res })
+
+@app.route('/get_scores', methods=['POST'])
+def get_scores():
+    score = int(request.json['score'])
+    highscore = session.get('highscore', 0)
+    session['highscore'] = max(score, highscore)
+    play_throughs = session.get('play-thr', 0)
+    session['play_thr'] += 1
+    return jsonify(highscore)
--- static/script.js
+++ static/script.js
@@ -1,6 +1,65 @@
 
-$('form').on('click', '#submit', (e) => {
+let score = 0;
+
+$('form').on('click', '#submit', handleClick);
+
+async function handleClick(e){
     e.preventDefault();
-    guess = $('#guess').val();
-    axios.post(`/words`, guess);
-});
+    const $guess = $('#guess');
+    const guess = $guess.val();
+    $guess.val('');
+    const response = await axios.get('/check_guess', {params: { guess: guess }})
+    const msg = response.data.result; 
+    console.log(msg);
+    msgUI(msg);
+    scoreKeeper(guess, msg);
+}
+
+function msgUI(msg){
+    const $msg = $('#msg')
+    if (msg === 'ok'){
+        $msg.text('OK!')
+    } 
+    else if (msg === 'not-on-board'){
+        $msg.text('NOT ON BOARD');
+    } 
+    else if (msg === 'not-word'){
+        $msg.text('NOT WORD');
+    }
+}
+
+function scoreKeeper(word, msg){
+    if (msg === 'ok'){
+        const points = word.length;
+        score += points;
+    }
+    $('#score').text(`Score: ${score}`);
+}
+
+function timer(){
+    let time = 3;
+    const interID = setInterval(() => {
+        time -= 1;
+        $('#timer').text(`Time Remaining: ${time}`);
+        if (time === 0) {
+            disableButton();
+            clearInterval(interID);
+            gameOver();
+        }
+    }, 1000)
+}
+
+function disableButton() {
+    $('#submit').attr('disabled', 'true');
+};
+
+async function gameOver(){
+    await axios.post('/get_scores', {score : score})
+
+}
+
+// starts timer
+const intID = setTimeout(() => {
+    timer();
+    clearTimeout(intID)
+}, 2000)
