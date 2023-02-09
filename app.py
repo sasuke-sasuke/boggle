b79ddb348ce00a8dd2531e6a8d85d7a83364e70b
Jeof Petty <jeofpetty@icloud.com>
2023-02-08 22:52:21 -0500

    fixed highscore and plays to update on page after timer

--- app.py
+++ app.py
@@ -8,28 +8,49 @@ app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 debug = DebugToolbarExtension(app)
 
 boggle_game = Boggle()
-#game_board = boggle_game.make_board()
+guessed_words = []
 
 @app.route('/')
 def index():
     game_board = boggle_game.make_board()
     session['boggle_board'] = game_board
     highscore = session.get('highscore', 0)
-    play_throughs = session.get('play_thr', 0)
-    return render_template('index.html', game_board=game_board, highscore=highscore, plays=play_throughs)
+    plays = session.get('plays', 0)
+    return render_template('index.html', game_board=game_board, highscore=highscore, plays=plays)
 
 @app.route('/check_guess')
 def check_guess():
     guess = request.args['guess']
     game_board = session['boggle_board']
-    res = boggle_game.check_valid_word(game_board, guess)
-    return jsonify({ 'result': res })
+    return validGuess(guess, game_board)
+    
 
 @app.route('/get_scores', methods=['POST'])
 def get_scores():
     score = int(request.json['score'])
+    highscore = save_high_score(score)
+    plays = save_play_throughs()
+    return jsonify({'highscore': highscore, 'plays': plays})
+
+
+def save_play_throughs():
+    if 'plays' in session:
+        session['plays'] += 1
+    else:
+        session['plays'] = 1
+    return session['plays']
+
+def save_high_score(score):
     highscore = session.get('highscore', 0)
     session['highscore'] = max(score, highscore)
-    play_throughs = session.get('play-thr', 0)
-    session['play_thr'] += 1
-    return jsonify(highscore)
+    return session['highscore']
+
+def validGuess(guess, board):
+    if len(guess) == 1:
+        return jsonify({'result': 'single-char'})
+    elif guess in guessed_words:
+        return jsonify({'result': 'already-guessed'})
+    elif guess not in guessed_words:
+        guessed_words.append(guess)
+        res = boggle_game.check_valid_word(board, guess)
+        return jsonify({ 'result': res })
--- static/script.js
+++ static/script.js
@@ -1,8 +1,13 @@
 
 let score = 0;
+let time = 5;
 
 $('form').on('click', '#submit', handleClick);
 
+/* 
+handleClick stops form refresh on click, gets guessed word, clear form value, 
+sends get request to check guess validity, calls msgUI, scoreKeeper
+*/
 async function handleClick(e){
     e.preventDefault();
     const $guess = $('#guess');
@@ -10,11 +15,11 @@ async function handleClick(e){
     $guess.val('');
     const response = await axios.get('/check_guess', {params: { guess: guess }})
     const msg = response.data.result; 
-    console.log(msg);
     msgUI(msg);
     scoreKeeper(guess, msg);
 }
 
+// Checks response message updates msg UI
 function msgUI(msg){
     const $msg = $('#msg')
     if (msg === 'ok'){
@@ -26,8 +31,15 @@ function msgUI(msg){
     else if (msg === 'not-word'){
         $msg.text('NOT WORD');
     }
+    else if (msg === 'already-guessed'){
+        $msg.text('WORD ALREADY GUESSED')
+    }
+    else if (msg === 'single-char'){
+        $msg.text('SIZE OF WORD MUST BE < 1')
+    }
 }
 
+// Calculates points for correct guess and adds to score
 function scoreKeeper(word, msg){
     if (msg === 'ok'){
         const points = word.length;
@@ -36,26 +48,29 @@ function scoreKeeper(word, msg){
     $('#score').text(`Score: ${score}`);
 }
 
+// Runs game timer and calls gameOver()
 function timer(){
-    let time = 3;
     const interID = setInterval(() => {
         time -= 1;
         $('#timer').text(`Time Remaining: ${time}`);
         if (time === 0) {
-            disableButton();
             clearInterval(interID);
             gameOver();
         }
     }, 1000)
 }
 
+// Disables to submit button
 function disableButton() {
     $('#submit').attr('disabled', 'true');
 };
 
+// Sends score data to server
 async function gameOver(){
-    await axios.post('/get_scores', {score : score})
-
+    const res = await axios.post('/get_scores', {score : score});
+    disableButton();
+    $('#highscore').text(`Highscore: ${res.data.highscore}`)
+    $('#num-plays').text(`Times played: ${res.data.plays}`)
 }
 
 // starts timer
